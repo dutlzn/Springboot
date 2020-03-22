7c316ab0c8bfb3f0fe25bd86b5051edb2700f238
dutlzn <2436013662@qq.com>
2020-03-23 00:11:15 +0800

    修改性别单选框bug

--- friday/demo/src/main/java/sys/demo/controller/TestController.java
+++ friday/demo/src/main/java/sys/demo/controller/TestController.java
@@ -3,12 +3,18 @@ package sys.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("")
 public class TestController {
-//    @GetMapping("testIndex")
-//    public String testIndex() {
-//        return "index";
-//    }
+    @GetMapping("testIndex")
+    /*
+    ?name= 是直接可以解析的
+     */
+    public String testIndex(String name,String var2) {
+        System.out.println(var2);
+        System.out.println(name);
+        return "test"+name+var2;
+    }
 }
