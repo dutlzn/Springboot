b95ddd91000b4b53214aecdaf6294f1f41954001
dutlzn <2436013662@qq.com>
2020-04-07 19:47:25 +0800

    自定义登陆页面和登录表单

---
+++ springsecuriy/dutlzn-security-parent/dutlzn-security-core/src/main/java/com/dutlzn/config/SpringSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.dutlzn.config;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//       数据库存储的密码一定是加密之后的
+        String password = passwordEncoder().encode("1234");
+        logger.info("加密之后存储的密码：" + password);
+        auth.inMemoryAuthentication().withUser("admin")
+                .password(password).authorities("admin");
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic()
+        http.formLogin()
+                .loginPage("/login/page")
+                .loginProcessingUrl("/login/form")
+                .usernameParameter("name")
+                .passwordParameter("pwd")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login/page").permitAll()
+                .anyRequest().authenticated()
+                ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/dist/**", "/modules/**", "/plugins/**");
+
+    }
+}
